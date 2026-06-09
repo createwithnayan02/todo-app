e5a22e3fa59e7ee8aee8e9f983d46541298afa72
createwithnayan02 <nayansmishra2006@gmail.com>
2026-06-09 21:30:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,45 @@
 from flask import Flask, render_template, request, redirect
+from datetime import datetime
 
 app = Flask(__name__)
 
 FILE_NAME = "text.txt"
 
+@app.route("/home")
+def home():
+    tasks = read_tasks()
+    return render_template("home.html", tasks=tasks)
+
 
 # Read tasks from file
 def read_tasks():
-    with open(FILE_NAME, "r") as file:
-        return file.readlines()
+    try:
+        with open(FILE_NAME, "r") as file:
+            return file.readlines()
+    except FileNotFoundError:
+        return []
 
 
-# Write one task to file
+# Write task to file
 def add_task_to_file(task):
     with open(FILE_NAME, "a") as file:
         file.write(task + "\n")
 
-
+#splash 
 @app.route("/")
-def index():
-    tasks = read_tasks()
-    tasks = [task.strip() for task in tasks]  # remove \n
-    return render_template("index.html", tasks=tasks)
+def splash():
+    return render_template("splash.html")
 
-
+#add task
 @app.route("/add", methods=["POST"])
 def add():
     task = request.form.get("task")
     if task:
-        add_task_to_file(task)
-    return redirect("/")
-    
+        date = datetime.now().strftime("%Y-%m-%d")
+        new_task = f"{task}|pending|{date}"
+        add_task_to_file(new_task)
+    return redirect("/home")
 
-if __name__ == "__main__":
+if __name__ == "__main__" :
     app.run(debug=True)
+
